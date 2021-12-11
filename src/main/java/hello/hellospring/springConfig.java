7fe7ce483ce824b4d52ee5fad507498402803dbf
wlroh <wlroh.paul@gmail.com>
2021-12-11 15:11:27 +0900

    commit

--- src/main/java/hello/hellospring/springConfig.java
+++ src/main/java/hello/hellospring/springConfig.java
@@ -1,22 +1,22 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class springConfig {
 
-    private DataSource dataSource;
+    EntityManager em;
 
-    public springConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public springConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -28,6 +28,7 @@ public class springConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
